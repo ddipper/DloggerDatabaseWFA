ee800deb905ff3f81962d7403bb933e98e15397d
Dmitry <dimonbyt@gmail.com>
2023-09-10 21:18:18 +0300

    add design

--- DloggerDatabaseWFA/Form1.Designer.cs
+++ DloggerDatabaseWFA/Form1.Designer.cs
@@ -29,22 +29,220 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbId = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.lbTime = new System.Windows.Forms.Label();
+            this.cmbAction = new System.Windows.Forms.ComboBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cmbClub = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(123, 102);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(168, 58);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "ID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(142, 145);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Time";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(123, 187);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Action";
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(12, 3);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(453, 33);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Dlogger database control system";
+            // 
+            // tbId
+            // 
+            this.tbId.Location = new System.Drawing.Point(215, 57);
+            this.tbId.Name = "tbId";
+            this.tbId.PlaceholderText = "123456789";
+            this.tbId.Size = new System.Drawing.Size(150, 27);
+            this.tbId.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(484, 41);
+            this.panel1.TabIndex = 6;
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(215, 102);
+            this.tbName.Name = "tbName";
+            this.tbName.PlaceholderText = "Dmitry";
+            this.tbName.Size = new System.Drawing.Size(150, 27);
+            this.tbName.TabIndex = 7;
+            // 
+            // lbTime
+            // 
+            this.lbTime.AutoSize = true;
+            this.lbTime.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbTime.Location = new System.Drawing.Point(212, 147);
+            this.lbTime.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbTime.Name = "lbTime";
+            this.lbTime.Size = new System.Drawing.Size(189, 22);
+            this.lbTime.TabIndex = 8;
+            this.lbTime.Text = "24.12.2023 00:00:00";
+            // 
+            // cmbAction
+            // 
+            this.cmbAction.FormattingEnabled = true;
+            this.cmbAction.Items.AddRange(new object[] {
+            "Вошел",
+            "Вышел"});
+            this.cmbAction.Location = new System.Drawing.Point(215, 186);
+            this.cmbAction.Name = "cmbAction";
+            this.cmbAction.Size = new System.Drawing.Size(150, 29);
+            this.cmbAction.TabIndex = 9;
+            this.cmbAction.Text = "Select action -->";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(176, 277);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(104, 32);
+            this.btnSave.TabIndex = 10;
+            this.btnSave.Text = "Upload";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cmbClub
+            // 
+            this.cmbClub.FormattingEnabled = true;
+            this.cmbClub.Items.AddRange(new object[] {
+            "Пушкинская",
+            "Серебрянка",
+            "Чижовка",
+            "Шабаны",
+            "Каменка"});
+            this.cmbClub.Location = new System.Drawing.Point(215, 232);
+            this.cmbClub.Name = "cmbClub";
+            this.cmbClub.Size = new System.Drawing.Size(150, 29);
+            this.cmbClub.TabIndex = 12;
+            this.cmbClub.Text = "Select a club -->";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(123, 233);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 24);
+            this.label6.TabIndex = 11;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(123, 237);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(59, 24);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Club";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(484, 320);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.cmbClub);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.cmbAction);
+            this.Controls.Add(this.lbTime);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.tbId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Dlogger Database";
+            this.Text = "cvvccfvc";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private TextBox tbId;
+        private Panel panel1;
+        private TextBox tbName;
+        private Label lbTime;
+        private ComboBox cmbAction;
+        private Button btnSave;
+        private ComboBox cmbClub;
+        private Label label6;
+        private Label label7;
     }
 }
--- DloggerDatabaseWFA/Form1.cs
+++ DloggerDatabaseWFA/Form1.cs
@@ -1,3 +1,5 @@
+using System.IO;
+
 namespace DloggerDatabaseWFA
 {
     public partial class Form1 : Form
@@ -5,6 +7,45 @@ namespace DloggerDatabaseWFA
         public Form1()
         {
             InitializeComponent();
+
+            lbTime.Text = DateTime.Now.ToString();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var id = tbId.Text;
+            var name = tbName.Text;
+            var time = DateTime.Now.ToString();
+
+            try // try for "Action"
+            {
+                var action = cmbAction.SelectedItem.ToString();
+                var club = cmbClub.SelectedItem.ToString();
+                MessageBox.Show($@"Save on D:\Prog\Log.txt", "ddipper");
+                WriteLog(id, name, action, club);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Select action or club", "System");
+            }
+        }
+
+        private void WriteLog(string id, string name, string action, string club)
+        {
+            if (id == String.Empty)
+                id = "none";
+            if (name == String.Empty)
+                name = "none";
+
+            string log = $"Id: {id}\n" +
+                         $"Name: {name}\n" +
+                         $"Time: {DateTime.Now}\n" +
+                         $"Action: {action}\n" +
+                         $"Club: {club}";
+
+            string path = @"D:\Prog\Logs\log.txt";
+
+            File.WriteAllText(path, log);
         }
     }
 }
